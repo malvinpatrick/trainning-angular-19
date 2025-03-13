fba0daf5aa9f520b117666206347995183fce97e
Malvin Patrick Kurniawan <malvin.p.kurniawan@devbz.local>
2025-03-13 23:37:31 +0700

    Update

--- src/app/declarative-programming/declarative-programming.component.ts
+++ src/app/declarative-programming/declarative-programming.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-declarative-programming',
-  imports: [CommonModule],
-  templateUrl: './declarative-programming.component.html',
-  styleUrl: './declarative-programming.component.css',
+    selector: 'app-declarative-programming',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './declarative-programming.component.html',
+    styleUrl: './declarative-programming.component.css'
 })
 export class DeclarativeProgrammingComponent {
-  isShow = true;
+    isShow = true;
+    employeeList = [
+        'Malvin', 
+        'Thomas', 
+        'Yusrin', 
+        'Eugenius'
+    ]
 
-  toogleShowAndHide() {
-    this.isShow = !this.isShow;
-  }
+    toogleShowAndHide() {
+        this.isShow = !this.isShow;
+    }
+
+    addEmployee(name: string) {
+        this.employeeList.push(name);
+    }
 }
